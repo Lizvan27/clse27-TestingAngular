b0e766f8d2058597688051383d9ebd638bf1b391
Lizvan27 <lizvanmandy@gmail.com>
2023-01-26 12:15:36 -0500

    inicio

---
+++ src/app/componets/calcu/calcu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calcu',
+  templateUrl: './calcu.component.html',
+  styleUrls: ['./calcu.component.css']
+})
+export class CalcuComponent {
+
+}
---
+++ src/app/componets/componets.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalcuComponent } from './calcu/calcu.component';
+
+
+
+@NgModule({
+  declarations: [
+    CalcuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponetsModule { }
